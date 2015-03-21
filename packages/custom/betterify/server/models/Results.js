b5fff06a83321ebb04be01e757ede31885cd5397
mmagruder <mmagruder@us.ibm.com>
2015-03-21 16:16:49 -0400

    changing the fucking result

--- packages/custom/betterify/server/models/Results.js
+++ packages/custom/betterify/server/models/Results.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 var ResultSchema = new mongoose.Schema({
-	place: { type: mongoose.Schema.Types.ObjectId, ref: "Place" }
+	place: { type: mongoose.Schema.Types.ObjectId, ref: "Place" },
+	votes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Vote" }]
 });
 
 mongoose.model('Result', ResultSchema);
